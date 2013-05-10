207d7341d87b4a8d420a035302ad317b8871b144
roby <roby@adscale.co.nz>
2013-05-10 12:02:16 +1200

    added commons-cli parsing to the executable war

--- server/src/main/java/web/application/Application.java
+++ server/src/main/java/web/application/Application.java
@@ -1,21 +1,62 @@
 package web.application;
 
-import service.environment.model.ChildPropertyContainer;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
 import org.eclipse.jetty.webapp.WebAppContext;
+import service.environment.model.ChildPropertyContainer;
 
 import java.net.URL;
 import java.security.ProtectionDomain;
 
 public class Application {
 
+    public static final String DIR_OPT = "d";
+
+    public static final String PORT_OPT = "p";
+
+    public static final String HOST_OPT = "host";
+
+    public static final String HELP_OPT = "h";
+
+
     public static void main(String[] args) {
+        Options options = getOpts(args);
+        CommandLine commandLine = getOpts(args, options);
+        if (commandLine.hasOption(HELP_OPT)) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp( "java -jar server.war", options);
+        } else{
+            startServer(commandLine);
+        }
+    }
+
+
+    private static void startServer(CommandLine commandLine) {
+        String host = "127.0.0.1";
+        int port = 8888;
+        if (commandLine.hasOption(DIR_OPT)) {
+            String optionValue = commandLine.getOptionValue(DIR_OPT);
+            System.getProperties().put(Context.ROOT_DIR, optionValue);
+        }
+        if (commandLine.hasOption(PORT_OPT)) {
+            port = Integer.parseInt(commandLine.getOptionValue(PORT_OPT));
+        }
+        if (commandLine.hasOption(HOST_OPT)) {
+            host = commandLine.getOptionValue(HOST_OPT);
+        }
+
         Server server = new Server();
         Connector connector = new SelectChannelConnector();
-        connector.setPort(8888);
-        connector.setHost("127.0.0.1");
+        connector.setPort(port);
+        connector.setHost(host);
         server.addConnector(connector);
         server.setStopAtShutdown(true);
         WebAppContext context = new WebAppContext();
@@ -28,10 +69,40 @@ public class Application {
         try {
             server.start();
             server.join();
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             e.printStackTrace();
             System.exit(100);
         }
+    }
+
+
+    private static CommandLine getOpts(String[] args, Options commandLine) {
+        CommandLineParser parser = new GnuParser();
+        try {
+             return parser.parse(commandLine, args);
+        }
+        catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private static Options getOpts(String[] args) {
+        Option dir =
+                new Option(DIR_OPT, "history-directory", true, "directory where properties files will be persisted");
+        dir.setArgName("dir");
+        Option port = new Option(PORT_OPT, "port", true, "port the server will run on : 8888 default");
+        port.setArgName("port");
+        Option host = new Option(HOST_OPT, "host", true, "host the server will bind to : 127.0.0.1 default");
+        port.setArgName("host");
+        Option help = new Option(HELP_OPT, "help",false, "help yo");
+        Options options = new Options();
+        options.addOption(dir);
+        options.addOption(port);
+        options.addOption(host);
+        options.addOption(help);
+        return options;
     }
 
 }
--- server/src/main/java/web/application/Context.java
+++ server/src/main/java/web/application/Context.java
@@ -1,7 +1,9 @@
 package web.application;
 
+import com.google.common.base.Strings;
 import com.google.common.io.Files;
 import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +13,21 @@ import service.environment.SnapshottingEnvironmentService;
 import service.history.EnvironmentSnapshotService;
 import service.history.FileSystemEnvironmentSnapshotService;
 
+import java.io.File;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan
 public class Context {
 
     public static final String ENV_NAME = "env";
+
+    public static final String ROOT_DIR = "root.dir";
+
+    private static final String ROOT_DIR_PROP = "#{systemProperties['" + ROOT_DIR + "']}";
+
+    @Value(ROOT_DIR_PROP)
+    private String root;
 
 
     @Bean(autowire = Autowire.BY_NAME, name = ENV_NAME)
@@ -26,8 +37,21 @@ public class Context {
 
 
     public EnvironmentSnapshotService historyService() {
-        FileSystemEnvironmentSnapshotService service =
-                new FileSystemEnvironmentSnapshotService(Files.createTempDir());
-        return service;
+        if (Strings.isNullOrEmpty(root)) {
+            return new FileSystemEnvironmentSnapshotService(Files.createTempDir());
+        }
+        else {
+            File rootDir = new File(root);
+            if (!rootDir.exists()) {
+                throw new RuntimeException("provided rootdir did not exist");
+            }
+            else if (!rootDir.isDirectory()) {
+                throw new RuntimeException("provided rootdir is not a directory");
+            }
+            else if (!rootDir.canWrite()) {
+                throw new RuntimeException("provided rootdir is not writable");
+            }
+            return new FileSystemEnvironmentSnapshotService(rootDir);
+        }
     }
 }
